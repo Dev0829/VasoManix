bc6c72e293066fb1c6df34369387cc21ef806f40
Dev829 <goldencross126@gmail.com>
2021-12-19 13:58:36 -0500

    Fix header, wrapper-1

--- views/layouts/default/footer.php
+++ views/layouts/default/footer.php
@@ -4,7 +4,6 @@ use yii\helpers\Url;
 ?>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script src="/products/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="/products/owlcarousel/owl.carousel.min.js"></script>
 <?php 
 if(isset($data['page']) && in_array($data['page'], ['home', 'order', 'upsell'])) : ?>
   <script src="https://www.paypal.com/sdk/js?client-id=<?=UtilityHelper::getCustomParameters('paypal_client_id');?>&vault=true&disable-funding=credit,card,bancontact,blik,eps,giropay,mybank,ideal,p24,sepa,sofort,venmo" data-sdk-integration-source="button-factory"></script>
--- views/layouts/default/top.php
+++ views/layouts/default/top.php
@@ -1,25 +1,17 @@
 <?php
 use app\helpers\UtilityHelper;
 ?>
-<div class="superior-header">
-  <div class="header-top">
-    <div class="container">
-      <div class="nav-wrapper">
-        <div class="d-flex mr-30">
-          <p class="description white mt-15 mb-15">Call us at <span class="bold"><a class="white" href="tel:<?= UtilityHelper::getCustomParameters('SitePhone'); ?>"><?= UtilityHelper::getCustomParameters('SitePhone'); ?></a></span></p>
-        </div>
-        <div class="d-flex">
-          <img class="security" src="/products/images/order/security.png">
-        </div>
-      </div>
+<header class="header bg-black">
+  <div class="container d-flex justify-content-between align-center">
+    <a class="d-flex" href="/products/">
+      <img class="logo" src="/products/images/logo.png">
+    </a>
+    <div class="nav-wrapper d-flex align-center">
+      <a href="/products/">HOME</a>
+      <a href="/products/">HOW IT WORKS</a>
+      <a href="/products/">FAQ</a>
+      <a href="/products/">GUARANTEE</a>
+      <a href="/products/">ORDER</a>
     </div>
   </div>
-  <div class="header-bottom">
-    <div class="container d-flex justify-content-between align-center">
-      <a class="d-flex" href="/products/">
-        <img class="logo" src="/products/images/order/logo.png">
-      </a>
-      <a class="d-flex cta_pane" href="/products/order/"><button class="cta_btn">Order Now</button></a>
-    </div>
-  </div>
-</div>
+</header>
